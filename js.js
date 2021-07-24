a68c4ebb774132c9d5978ae5ebf1ab7e95c14b30
Anna18168 <87442257+Anna18168@users.noreply.github.com>
2021-07-24 13:53:58 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,53 +1,96 @@
-let day = Math.floor(Math.random() * 30) + 1; // числа от 1 до 31
+function decada(day){
+    if (day > 0 && day < 11) {
+        console.log('first');
+    }
+    else if(day > 10 && day < 20) {
+        console.log('second');
+    }
+    else  {
+        console.log('third');
+    }
 
-if (day > 0 && day < 11) {
-    console.log('first');
 }
-else if(day > 10 && day < 20) {
-    console.log('second');
+decada(24);
+
+
+let a = function (month){
+    const condition = month === 1|| month === 2 || month === 12;
+
+    if (condition) {
+        console.log('winter');
+    }
+    else if(month >2 && month < 6) {
+        console.log('spring');
+    }
+    else if(month > 5 && month >9)  {
+        console.log('summer');
+    }
+    else  {
+        console.log('autum');
+    }
 }
-else  {
-    console.log('third');
+a(5);
+
+
+let part =(min) =>{
+
+    if (min > -1 && min < 16) {
+        console.log('first');
+    }
+    else if(min > 15 && min < 31) {
+        console.log('second');
+    }
+    else if(min > 30 && min < 46)  {
+        console.log('third');
+    }
+    else  {
+        console.log('fourth');
+    }
+}
+part(35)
+
+
+function checkAge(age) {
+    console.log((age > 18) ? 'true' : 'false')
+}
+checkAge('19')
+
+
+// Сумма чисел от 0 до 100
+let c=0;
+for (let i=0; i<=100; i++){
+  c += i;
+}
+console.log(c);
+
+let b = 0;
+let i = 0;
+while (i<=100){
+    b += i;
+    i++;
+}
+console.log(b);
+
+
+let d = 0;
+let h = 0;
+do {
+    d += h;
+    h++;
+} while (h <= 100)
+console.log(d);
+
+
+
+function product(start,end){
+    let x=1;
+    for( let i=start; i<= end; i++){
+        if(i % 2 === 0){
+            x *= i;
+
+        }
+    }
+    return x;
 }
 
-
-
-
-let month = Math.floor(Math.random() * 11) + 1; // числа от 1 до 12
-
-const condition = month === 1|| month === 2 || month === 12;
-
-if (condition) {
-    console.log('winter');
-}
-else if(month >2 && month < 6) {
-    console.log('spring');
-}
-else if(month > 5 && month >9)  {
-    console.log('summer');
-}
-else  {
-    console.log('autum');
-}
-
-
-let min = Math.floor(Math.random() * 59) ; // числа от 0 до 59
-
-if (min > -1 && min < 16) {
-    console.log('first');
-}
-else if(min > 15 && min < 31) {
-    console.log('second');
-}
-else if(min > 30 && min > 46)  {
-    console.log('third');
-}
-else  {
-    console.log('fourth');
-}
-
-let age = 9;
-
-// age < 18 ?  console.log('child')  : console.log('adult');
-
-console.log (age < 18 ? 'child' : 'adult');
+console.log(product(1,6))
