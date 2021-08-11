8417b5b548a91e098ca8190374bd0420ecea1193
Anna18168 <87442257+Anna18168@users.noreply.github.com>
2021-08-11 16:24:30 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,57 +1,76 @@
-let arr = [1, 2, 3, 4, 5];
+let obj1= {
+    a:1,
+    b:2,
+};
 
-console.log(arr)
-console.log(arr.slice(0,3));
-let arr2 = arr.slice(0,3);
-console.log(arr2)
+let obj2= {
+    a:1,
+    b:2,
+};
+let obj3= {
+    a:3,
+    b:4,
+};
 
+const isObj = (firstObj, secondObj) => {
+    const firstObjKeys = Object.keys(firstObj);
+    const secondObjKeys = Object.keys(secondObj);
 
-console.log(arr.slice(3,5));
-let arr3 = arr.slice(3,5);
-console.log(arr3)
+    if (firstObjKeys.length !== secondObjKeys.length) {
+        return false;
+    }
 
-
-
-
-const num ='1 1 2 4 4 5 6 7 1 2 4 5 3';
-let arr4 = num.split(' ');
-console.log(arr4)
-
-function spliceNum(numm,symbol){
- let a= "";
- for (let i=0; i < numm.length; i++){
-  if (numm[i]%2 !== 0 && i < numm.length-1){
-   a += numm[i] + symbol;
-  }
-  else {
-   a += numm[i];
-  }
- }
- console.log(a);
- }
-
-spliceNum(arr4, '-')
-
-
-const arr5 = [1,2,3,4,5,6,7,8];
-const arr6 = ['Roma', 'Sasha', 'Vlad', 'Oleg'];
-const wrongLengthArr = [1,2,3];
-
-function changeHalf(arr7){
- let c =[];
- if(arr7.length%2 === 0 && (arr7.length)/2) {
-  c = arr7.splice((arr7.length)/2);
-  console.log(c);
-
-  let b = c.concat(arr7);
-  console.log(b);
- }
-  else if (arr7.length%2 !== 0){
-   console.log(" Нечетная длина массива")
-  }
-
-
+    for (const key in firstObj) {    //Аналог Object.keys();
+        if (firstObj[key] !== secondObj[key]) {
+            return false;
+        }
+    }
+    return true;
 }
-changeHalf(arr5)
-changeHalf(arr6)
-changeHalf(wrongLengthArr)
+console.log(isObj(obj1 , obj2));
+
+// Аналог Object.values();
+
+let obj= {
+    a:1,
+    b:2,
+};
+let x = 0;
+ for (let key in obj) {
+    x = obj[key];
+    
+ }
+console.log(x)
+
+
+
+
+
+function Animal (aObj){
+     Object.assign(this,aObj );
+}
+
+ const cat = new Animal({
+     name: 'cat',
+     location: 'Ukrain',
+      food: 'fish',
+ });
+
+const dog = new Animal({
+    name: 'dog',
+    location: 'Germany',
+    food: 'meet',
+});
+
+const fish = new Animal({
+    name: 'fish',
+    location: 'Spain',
+    food: 'seaweed',
+});
+const zoo = [cat, dog, fish];
+const filtered = zoo.filter( (animal) =>{
+    return animal.location === 'Spain';
+})
+
+console.log(zoo);
+console.log(filtered);
